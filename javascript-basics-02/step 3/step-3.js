b14346c6a394ea8dc862b97fe6095d320330e0e2
abdulaziz <aboudecharkawi@gmail.com>
2023-08-31 01:11:18 +0300

    JS2 step3

---
+++ javascript-basics-02/step 3/step-3.js
@@ -0,0 +1,10 @@
+const displayDiv = document.querySelector('#displayDiv');
+    
+
+const input = document.querySelector('#name');
+
+
+input.addEventListener('input', function() {
+  
+  displayDiv.textContent = input.value;
+});
