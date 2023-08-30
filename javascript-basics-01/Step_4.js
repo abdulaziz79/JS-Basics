deadc8b4c1c139bd704a2a8f4d6c4f410b92a1d3
abdulaziz <aboudecharkawi@gmail.com>
2023-08-30 10:43:25 +0300

    step-4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,8 @@
+var name = prompt("Please enter your name:");
+var surname = prompt("Please enter your surname:");
+var city = prompt("Please enter your city:");
+
+
+var greeting = "Hello, " + name + " " + surname + " from " + city + "!";
+
+alert(greeting);
