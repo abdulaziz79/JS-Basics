64833522c696ce44db10778f6328f88e3f879814
abdulaziz <aboudecharkawi@gmail.com>
2023-08-30 10:38:39 +0300

    step-3

---
+++ javascript-basics-01/Step_2.js
@@ -0,0 +1,4 @@
+var name = "Aziz";
+var surname = "cherkawi";
+var city = "Tripoli";
+alert("Name: " + name + "\nSurname: " + surname + "\nCity: " + city);
---
+++ javascript-basics-01/Step_3.js
@@ -0,0 +1,8 @@
+var userName = prompt("Please enter your name:");
+
+
+if (userName) {
+  alert("Hello, " + userName);
+} else {
+  alert("Hello, Guest");
+}
