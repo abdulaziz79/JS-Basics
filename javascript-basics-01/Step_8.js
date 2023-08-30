91cd1da0396781a6606185d3f1d3ec423e11e6ed
abdulaziz <aboudecharkawi@gmail.com>
2023-08-30 14:03:11 +0300

    step-8

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,12 @@
+document.getElementById("validate").addEventListener("click", function(event) {
+    event.preventDefault();
+    var ageInput = document.getElementById("age").value;
+    var resultDiv = document.getElementById("result");
+
+    if (parseInt(ageInput) > 18) {
+        alert("you are over 18 ")
+
+    } else {
+        alert( "You are under 18");
+    }
+});
